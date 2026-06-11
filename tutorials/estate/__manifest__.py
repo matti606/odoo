46a20a6e3a6d7687e7efa9b613aec6539265de7b
matti606 <56224489+matti606@users.noreply.github.com>
2026-06-11 15:10:44 +0200

    [IMP] estate: add property action

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -1,6 +1,9 @@
 {
     'name': 'Estate',
     'depends': ['base'],
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'views/estate_property_views.xml',
+        'security/ir.model.access.csv'
+        ],
     'application': True
 }
