aec5e1dd8579826050b3c451ca67681dadef9aa3
matti606 <56224489+matti606@users.noreply.github.com>
2026-06-11 17:43:05 +0200

    [IMP] estate: set default value for property fields

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields
 
 
@@ -8,15 +9,18 @@ class EstateProperty(models.Model):
     name = fields.Char('Title', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available from', copy=False)
+    date_availability = fields.Date(
+        'Available from',
+        copy=False,
+        default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float('Expected price', required=True)
     selling_price = fields.Float('Selling price', readonly=True, copy=False)
-    bedrooms = fields.Integer('Bedrooms')
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden area')
+    garden_area = fields.Integer('Garden area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
