24547d3cc5f5fac37f18e7994cecfad399b5aeea
matti606 <56224489+matti606@users.noreply.github.com>
2026-06-17 18:59:07 +0200

    [IMP] estate: add widget options for property view

--- tutorials/estate/models/estate_property_tag.py
+++ tutorials/estate/models/estate_property_tag.py
@@ -7,6 +7,7 @@ class EstatePropertyTag(models.Model):
     _order = 'name'
 
     name = fields.Char('Name', required=True)
+    color = fields.Integer('Color')
 
     _sql_constraints = [
         (
