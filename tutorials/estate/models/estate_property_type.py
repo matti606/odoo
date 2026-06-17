15b2ff98ea895588705ba8312b43e4b910bd3d3b
matti606 <56224489+matti606@users.noreply.github.com>
2026-06-17 18:31:31 +0200

    [IMP] estate: add manual ordering for property type

--- tutorials/estate/models/estate_property_type.py
+++ tutorials/estate/models/estate_property_type.py
@@ -4,10 +4,14 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Estate Property Type'
-    _order = 'name'
+    _order = 'sequence, name'
 
     name = fields.Char('Name', required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
+    sequence = fields.Integer(
+        'Sequence', default=1,
+        help='Used to order stages. Lower is better.'
+    )
 
     _sql_constraints = [
         (
