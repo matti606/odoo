c258067cf9f8443e903e988b29d78d9b4b33ab1b
matti606 <56224489+matti606@users.noreply.github.com>
2026-06-19 17:42:32 +0200

    [IMP] estate_account: create invoice when property is sold

--- tutorials/estate_account/models/estate_property.py
+++ tutorials/estate_account/models/estate_property.py
@@ -5,5 +5,9 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_set_sold(self):
-        print('this should work, this is the link module')
+        for record in self:
+            self.env['account.move'].create({
+                'partner_id': record.buyer.id,
+                'move_type': 'out_invoice'
+            })
         return super().action_set_sold()
